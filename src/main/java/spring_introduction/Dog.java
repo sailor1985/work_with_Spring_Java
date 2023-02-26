f860c2210242c957468e6ab9f88cd65a35ab9c4e
Michael <cadetvire@rambler.ru>
2023-02-26 23:09:52 +0300

    Spring, 1. Рассмотрение IoC и DI, до видео 17 включительно

---
+++ src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,30 @@
+package spring_introduction;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Dog implements Pet {
+//    private String name;
+
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+    public void init(){
+        System.out.println("Class Dog: init method");
+    }
+    public void destroy(){
+        System.out.println("Class Dog: destroy method");
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    @Override
+    public void say(){
+        System.out.println("Bow-Wow");
+    }
+}
